6288a1080a621910e6b5cc2f009409af74c58f0b
Guenfoudi-Mohamed <mohamed.guenfoudi@hotmail.com>
2022-10-04 19:30:03 +0100

    Update app_32.js

--- app_32.js
+++ app_32.js
@@ -2,36 +2,72 @@
 
 
 const arr = [];
+let stop;
+
+// set bar Value
 function setBarValue(){
     arr.length = 0;
     for(let i = 0;i<10;i++){
         arr.push(Math.ceil(Math.random() * 10));
     }
     displayBar(arr);
+    if(!isNaN(stop)){clearInterval(stop);}
 }
 
+// function btn sort bars
 function btnSort(){
     sortBarValue(arr);
-    displayBar(arr);
 }
 
+
+// btn sort bars
 function sortBarValue(arr){
-    for(let i = 0;i<arr.length-1;i++){
-        for(let x = i+1;x<arr.length;x++){
-            if(arr[i]>arr[x]){
-                [arr[x],arr[i]]=[arr[i],arr[x]];
-            }
+    
+    const bars = document.querySelector('body #container .bars');
+    let i = 0;
+    let conteurClear = 0;
+    console.clear();
+    stop  = setInterval(function(){
+        let x = i+1;
+
+        bars.children[i].classList.add('active');
+        bars.children[x].classList.add('active');
+        
+        const I = i;
+        const X = x;
+        
+        if(Number(bars.children[i].heightValue) > Number(bars.children[x].heightValue)){
+            bars.insertBefore(bars.children[x],bars.children[i]);
+            conteurClear = 0;
         }
-    }
+        if(Number(bars.children[i].heightValue) <= Number(bars.children[x].heightValue)){
+            conteurClear++;
+        }
+        if(i == bars.childElementCount-2){
+            i = -1;
+        }
+        ++i;
+        if(conteurClear >= (bars.childElementCount-1)){clearInterval(stop);}
+        
+        setTimeout(function(){
+                bars.children[X].classList.remove('active');
+                bars.children[I].classList.remove('active');
+        },990);   
+
+    },1000);
+    i = 0;
 }
+// display bars
 function displayBar(arr){
     document.querySelector('body #container .bars').innerHTML = '';
     for(let i = 0;i<arr.length;i++){
         const bar = document.createElement('div');
         bar.className = 'bar';
         bar.style.setProperty('height',`${(arr[i] * 30)}px`);
+        bar.heightValue = `${arr[i] * 30}`;
         document.querySelector('body #container .bars').appendChild(bar);
     }
 };
-// console.log(window.matchMedia('(prefers-color-scheme: dark)').matches); detect mode dark or light ?!
+
+// console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);          detect mode dark or light ?!
 document.addEventListener('DOMContentLoaded',function(){setBarValue()});
